62c7e0a8d8dd60d0b29553112ca9aaf698d5e911
camkol <koliyus3@hotmail.com>
2024-04-17 00:56:31 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const [someDate, setSomeDate] = useState(new Date());
+
+  function stepPlus() {
+    setStep((s) => s + 1);
+  }
+  function stepMinus() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+  function countPlus() {
+    const newCount = count + step;
+    setCount(newCount);
+    updateDate(newCount);
+  }
+  function countMinus() {
+    if (count > 0) {
+      const newCount = count - step;
+      setCount(newCount);
+      updateDate(newCount);
+    }
+  }
+
+  const updateDate = (newCount) => {
+    const newDate = new Date(someDate);
+    let daysToAdd = newCount - count;
+    newDate.setDate(newDate.getDate() + daysToAdd);
+    setSomeDate(newDate);
+  };
+
+  return (
+    <div id="counter">
+      <p id="step">
+        <span onClick={stepMinus}>-</span>Step: {step}
+        <span onClick={stepPlus}>+</span>
+      </p>
+      <p style={{ marginTop: 0 }}>
+        <span onClick={countMinus}>-</span>Count: {count}
+        <span onClick={countPlus}>+</span>
+      </p>
+      <div id="date">
+        <p>Today is {someDate.toDateString()}</p>
+      </div>
+    </div>
+  );
+}
